d1a81fd939ad6bc04565038dbe5392a65e40f96d
edu <eduardomachucamoreno@gmail.com>
2023-05-19 17:10:02 +0200

    First commit

---
+++ src/DigitButton.jsx
@@ -0,0 +1,14 @@
+import { ACTIONS } from "./App"
+import PropTypes from 'prop-types';
+
+export default function DigitButton({ dispatch, digit }) {
+    return (
+        <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+        >{digit}
+        </button>
+    )
+}
+DigitButton.propTypes = {
+    dispatch: PropTypes.array,
+    digit: PropTypes.array,
+};
---
+++ src/OperationButton.jsx
@@ -0,0 +1,14 @@
+import { ACTIONS } from "./App"
+import PropTypes from 'prop-types';
+
+export default function OperationButton({ dispatch, operation }) {
+    return (
+        <button onClick={() => dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })}
+        >{operation}
+        </button>
+    )
+}
+OperationButton.propTypes = {
+    dispatch: PropTypes.array,
+    operation: PropTypes.array,
+};
